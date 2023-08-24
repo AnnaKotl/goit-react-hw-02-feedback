584c5df42c54d80b2a0c8aee953993e14d42d673
Анна Котляр <kotlyaranya1771@gmail.com>
2023-08-24 15:57:07 +0300

    huk

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { GlobalStyle } from './components/styles/GlobalStyle.js';
 import { Layout } from './components/styles/Layout.js';
 
@@ -9,66 +9,56 @@ import { Notification } from './components/Notification/Notification.js';
 import { Wrapper } from 'components/styles/Container.styled.js';
 import { CoffeeImage } from 'components/Coffe/Coffe.js';
 
-class App extends Component {
-  state = {
+export const App = () => {
+  const [data, setData] = useState({
     good: 0,
     neutral: 0,
     bad: 0
-  };
+  });
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
-    return good + neutral + bad;
-  };
-
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    return totalFeedback === 0 ? 0 : (good / totalFeedback) * 100;
-  };
-
-  handleFeedback = (feedbackType) => {
-  this.setState(prevState => ({ [feedbackType]: prevState[feedbackType] + 1 }));
-  };
-
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positivePercentage = this.countPositiveFeedbackPercentage();
+  const calcValues = () => {
+    const { good, neutral, bad } = data;
+    const totalFeedback = good + neutral + bad;
+    const positiveValue = totalFeedback === 0 ? 0 : (good / totalFeedback) * 100;
     const shouldPulsate = true;
 
-    return (
-      <Layout>
-        <Wrapper>
-          <Section title="Leave feedback" shouldpulsate={shouldPulsate.toString()}>
+    return { totalFeedback, positiveValue, shouldPulsate };
+  };
 
-            <FeedbackOptions
-              options={['good', 'neutral', 'bad']}
-              onLeaveFeedback={this.handleFeedback}
+  const handleFeedback = (feedbackType) => {
+    setData((prevData) => ({
+      ...prevData,
+      [feedbackType]: prevData[feedbackType] + 1
+    }));
+  };
+
+  const { totalFeedback, positiveValue, shouldPulsate } = calcValues();
+
+  return (
+    <Layout>
+      <Wrapper>
+        <Section title="Leave feedback" shouldpulsate={shouldPulsate.toString()}>
+          <FeedbackOptions options={['good', 'neutral', 'bad']} onLeaveFeedback={handleFeedback} />
+        </Section>
+
+        <Section title="Statistics">
+          {totalFeedback === 0 ? (
+            <>
+              <Notification message="There is no feedback" />
+              <CoffeeImage />
+            </>
+          ) : (
+            <Statistics
+              good={data.good}
+              neutral={data.neutral}
+              bad={data.bad}
+              total={totalFeedback}
+              positivePercentage={positiveValue}
             />
-          </Section>
-
-          <Section title="Statistics">
-            {totalFeedback === 0 ? (
-              <>
-                <Notification message="There is no feedback" />
-                <CoffeeImage/>
-              </>
-            ) : (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={totalFeedback}
-                positivePercentage={positivePercentage}
-              />
-            )}
-          </Section>
-        </Wrapper>
-        <GlobalStyle />
-      </Layout>
-    );
-  }
-}
-
-export default App;
+          )}
+        </Section>
+      </Wrapper>
+      <GlobalStyle />
+    </Layout>
+  );
+};
